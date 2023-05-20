c16fdf67df93c1a55d56e104937a000bd7e151e3
Bohdan Oleksa <bohdan.oleksa@gmail.com>
2023-05-20 14:27:23 +0200

    feat: created date piscker component

--- src/components/DatePicker.tsx
+++ src/components/DatePicker.tsx
@@ -1,35 +1,52 @@
 import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import DateTimePicker from '@react-native-community/datetimepicker';
+import DateTimePickerModal from 'react-native-modal-datetime-picker';
+import Icon from '@expo/vector-icons//MaterialIcons';
 
 export default function DatePicker() {
-  const [date, setDate] = useState(new Date());
+  const [selectedDate, setSelectedDate] = useState(new Date());
+  const [datePickerVisible, setDatePickerVisible] = useState(false);
 
-  const onChange = (event, selectedDate) => {
-    const currentDate = selectedDate;
-    setDate(currentDate);
+  const showDatePicker = () => {
+    setDatePickerVisible(true);
+  };
+
+  const hideDatePicker = () => {
+    setDatePickerVisible(false);
+  };
+
+  const handleConfirm = (date: Date) => {
+    setSelectedDate(date);
+    hideDatePicker();
   };
 
   return (
     <View style={styles.container}>
-      <DateTimePicker
-        testID="dateTimePicker"
-        value={date}
-        mode={'date'}
-        is24Hour={true}
-        onChange={onChange}
-        style={styles.picker}
+      <Text style={styles.pickerText} onPress={showDatePicker}>
+        {selectedDate ? selectedDate.toLocaleDateString() : 'No date selected'}
+        <Icon name="arrow-drop-down" size={16} color={'#2A3356'} />
+      </Text>
+
+      <DateTimePickerModal
+        date={selectedDate}
+        isVisible={datePickerVisible}
+        mode="date"
+        display="inline"
+        onConfirm={handleConfirm}
+        onCancel={hideDatePicker}
+        maximumDate={new Date()}
+        locale="en_GB"
       />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    // flex: 1,
-    // flexDirection: 'row',
+  container: {},
+  pickerText: {
+    fontSize: 16,
+    fontWeight: '500',
+    color: '#2A3356',
   },
-  picker: {
-    backgroundColor: '',
-  },
+  picker: {},
 });
