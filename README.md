955317fc0e1120e25efb6114329586d646b9ca7e
Bohdan Oleksa <bohdan.oleksa@gmail.com>
2023-06-05 19:03:49 +0200

    refactor: changed EnterValue buttons

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # Inex - mobile app for expenses
 
+# v1.0.2
+
+- changed notification text message
+- changed EnterValue buttons
+
 # v1.0.1
 
 - changed notification behavior
--- src/components/EnterValue.tsx
+++ src/components/EnterValue.tsx
@@ -2,8 +2,7 @@ import { Keyboard, StyleSheet, View, TouchableOpacity } from 'react-native';
 import React, { useCallback } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { IMainState, setSelectedTransactionType, setEnteredValue } from '../state/mainState';
-import Icon from '@expo/vector-icons/MaterialIcons';
-import { TextInput } from 'react-native-paper';
+import { TextInput, Text } from 'react-native-paper';
 import * as Haptics from 'expo-haptics';
 
 import { TransactionType } from '../repository/transactions';
@@ -24,11 +23,15 @@ export default function EnterValue() {
 
   return (
     <View style={styles.container}>
-      <TouchableOpacity onPress={() => handleChooseTransactionType('expenses')}>
-        <Icon
-          style={styles.expensesIcon}
-          name={selectedTransactionType === 'expenses' ? 'remove-circle' : 'remove-circle-outline'}
-        />
+      <TouchableOpacity
+        style={[
+          styles.transactionBtn,
+          styles.transactionLeftBtn,
+          selectedTransactionType === 'expenses' ? styles.activeTransactionBtn : {},
+        ]}
+        onPress={() => handleChooseTransactionType('expenses')}
+      >
+        <Text style={selectedTransactionType === 'expenses' ? styles.activeText : {}}>Expense</Text>
       </TouchableOpacity>
 
       <TextInput
@@ -42,11 +45,15 @@ export default function EnterValue() {
         onChangeText={handleEnterValue}
       />
 
-      <TouchableOpacity onPress={() => handleChooseTransactionType('incomes')}>
-        <Icon
-          style={styles.incomesIcon}
-          name={selectedTransactionType === 'incomes' ? 'add-circle' : 'add-circle-outline'}
-        />
+      <TouchableOpacity
+        style={[
+          styles.transactionBtn,
+          styles.transactionRightBtn,
+          selectedTransactionType === 'incomes' ? styles.activeTransactionBtn : {},
+        ]}
+        onPress={() => handleChooseTransactionType('incomes')}
+      >
+        <Text style={selectedTransactionType === 'incomes' ? styles.activeText : {}}>Income</Text>
       </TouchableOpacity>
     </View>
   );
@@ -56,8 +63,8 @@ const styles = StyleSheet.create({
   container: {
     width: '100%',
     flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
+    justifyContent: 'center',
+    alignItems: 'flex-end',
     paddingHorizontal: 15,
     paddingVertical: 24,
     marginTop: 8,
@@ -67,7 +74,8 @@ const styles = StyleSheet.create({
     width: 155,
     height: 56,
     backgroundColor: '#FFFFFF',
-    fontSize: 24,
+    fontSize: 16,
+    borderColor: 'red',
   },
   expensesIcon: {
     fontSize: 56,
@@ -76,5 +84,32 @@ const styles = StyleSheet.create({
   incomesIcon: {
     fontSize: 56,
     color: '#2E76B0',
+  },
+  transactionBtn: {
+    height: 58,
+    backgroundColor: '#FFFFFF',
+    width: 100,
+    borderWidth: 1,
+    borderColor: 'grey',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  transactionLeftBtn: {
+    borderTopLeftRadius: 30,
+    borderBottomLeftRadius: 30,
+    borderEndWidth: 0,
+  },
+  transactionRightBtn: {
+    borderTopRightRadius: 30,
+    borderBottomRightRadius: 30,
+    borderStartWidth: 0,
+  },
+  activeTransactionBtn: {
+    backgroundColor: '#2E76B0',
+    borderColor: '#2E76B0',
+  },
+  activeText: {
+    color: '#FFFFFF',
+    fontWeight: '500',
   },
 });
